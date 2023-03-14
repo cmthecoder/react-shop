421f651c1269bae1b34045f5309edae2bb805d3c
Chris Mems <christopher.mems@.com>
2023-03-14 17:08:25 -0500

    test links for routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Shop from './pages/Shop/Shop';
+import Cart from './pages/Cart/Cart';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route
+          path='/'
+          element={<Shop />}
+        />
+        <Route
+          path='/cart'
+          element={<Cart />}
+        />
+      </Routes>
     </div>
   );
 }
